c937e03cf9bbc8404961ac8f89c1eda0f01ac934
James Wright <james@jameswright.xyz>
2026-01-17 13:03:05 -0700

    Add .clang-format and format

--- ex49.c
+++ ex49.c
@@ -16,12 +16,11 @@ typedef struct {
 
 #define M_PI 3.1415926535897932384626433827950288
 
-static PetscErrorCode project_function(PetscInt dim, PetscReal time, const PetscReal x[], PetscInt Nc, PetscScalar *u,
-                                       void *ctx) {
+static PetscErrorCode project_function(PetscInt dim, PetscReal time, const PetscReal x[], PetscInt Nc, PetscScalar *u, void *ctx) {
   PetscReal x_tot = 0;
 
   PetscFunctionBeginUser;
-  for (PetscInt d = 0; d < dim; d++) x_tot += PetscSqr(x[d]) ;
+  for (PetscInt d = 0; d < dim; d++) x_tot += PetscSqr(x[d]);
   x_tot = sqrt(x_tot);
   for (PetscInt c = 0; c < Nc; c++) {
     PetscScalar value = sin(2.3 * M_PI * x_tot);
@@ -31,8 +30,7 @@ static PetscErrorCode project_function(PetscInt dim, PetscReal time, const Petsc
   PetscFunctionReturn(PETSC_SUCCESS);
 }
 
-static PetscErrorCode ProcessOptions(MPI_Comm comm, AppCtx *options)
-{
+static PetscErrorCode ProcessOptions(MPI_Comm comm, AppCtx *options) {
   PetscFunctionBeginUser;
   options->useFE          = PETSC_TRUE;
   options->degree         = 1;
@@ -43,16 +41,18 @@ static PetscErrorCode ProcessOptions(MPI_Comm comm, AppCtx *options)
   PetscCall(PetscOptionsBool("-use_fe", "Use FE or FV discretization", "ex49.c", options->useFE, &options->useFE, NULL));
   PetscCall(PetscOptionsInt("-degree", "Degree of FEM discretization", "ex49.c", options->degree, &options->degree, NULL));
   PetscCall(PetscOptionsInt("-num_comps", "Number of components", "ex49.c", options->num_comps, &options->num_comps, NULL));
-  PetscCall(PetscOptionsBool("-closure_tensor", "Use DMPlexSetClosurePermutationTensor()", "ex49.c", options->closure_tensor, &options->closure_tensor, NULL));
-  PetscCall(PetscOptionsBool("-project_solution", "Project solution onto mesh", "ex49.c", options->project_solution, &options->project_solution, NULL));
-  PetscCall(PetscOptionsBool("-set_partition_balance", "Set partition balance", "DMPlexSetPartitionBalance", options->part_balance, &options->part_balance, NULL));
+  PetscCall(PetscOptionsBool("-closure_tensor", "Use DMPlexSetClosurePermutationTensor()", "ex49.c", options->closure_tensor,
+                             &options->closure_tensor, NULL));
+  PetscCall(PetscOptionsBool("-project_solution", "Project solution onto mesh", "ex49.c", options->project_solution, &options->project_solution,
+                             NULL));
+  PetscCall(PetscOptionsBool("-set_partition_balance", "Set partition balance", "DMPlexSetPartitionBalance", options->part_balance,
+                             &options->part_balance, NULL));
   PetscCall(PetscOptionsBool("-create_mat", "Create Mat", "DMCreateMatrix", options->create_mat, &options->create_mat, NULL));
   PetscOptionsEnd();
   PetscFunctionReturn(PETSC_SUCCESS);
 }
 
-static PetscErrorCode CreateMesh(MPI_Comm comm, AppCtx *user, DM *dm)
-{
+static PetscErrorCode CreateMesh(MPI_Comm comm, AppCtx *user, DM *dm) {
   PetscFunctionBeginUser;
   PetscCall(DMCreate(comm, dm));
   PetscCall(DMSetType(*dm, DMPLEX));
@@ -63,8 +63,7 @@ static PetscErrorCode CreateMesh(MPI_Comm comm, AppCtx *user, DM *dm)
   PetscFunctionReturn(PETSC_SUCCESS);
 }
 
-static PetscErrorCode SetupDiscretization(DM dm, AppCtx *user)
-{
+static PetscErrorCode SetupDiscretization(DM dm, AppCtx *user) {
   DM       cdm = dm;
   PetscInt dim;
   MPI_Comm comm = PetscObjectComm((PetscObject)dm);
@@ -72,7 +71,7 @@ static PetscErrorCode SetupDiscretization(DM dm, AppCtx *user)
   PetscFunctionBeginUser;
   PetscCall(DMGetDimension(dm, &dim));
 
-  PetscFE        fe, fe_face;
+  PetscFE fe, fe_face;
 
   PetscCall(PetscFECreateLagrange(comm, dim, user->num_comps, PETSC_FALSE, user->degree, user->degree, &fe));
   PetscCall(PetscFEGetHeightSubspace(fe, 1, &fe_face));
@@ -114,7 +113,7 @@ PetscErrorCode DMGetGlobalVectorInfo(DM dm, PetscInt *local_size, PetscInt *glob
 }
 
 PetscErrorCode DMPrintVecSizes(DM dm, AppCtx *user) {
-  MPI_Comm comm = PetscObjectComm((PetscObject)dm);
+  MPI_Comm    comm = PetscObjectComm((PetscObject)dm);
   PetscMPIInt rank, comm_size;
 
   PetscFunctionBeginUser;
@@ -134,7 +133,7 @@ PetscErrorCode DMPrintVecSizes(DM dm, AppCtx *user) {
     PetscInt  median_index = comm_size % 2 ? comm_size / 2 : comm_size / 2 - 1;
     if (!rank) PetscCall(PetscMalloc1(comm_size, &gather_buffer));
 
-    PetscCall(PetscPrintf( comm, "  Total Global Dofs                  : %" PetscInt_FMT "\n", global_dofs));
+    PetscCall(PetscPrintf(comm, "  Total Global Dofs                  : %" PetscInt_FMT "\n", global_dofs));
 
     PetscCallMPI(MPI_Gather(&owned_dofs, 1, MPIU_INT, gather_buffer, 1, MPIU_INT, 0, comm));
     if (!rank) {
@@ -143,9 +142,10 @@ PetscErrorCode DMPrintVecSizes(DM dm, AppCtx *user) {
       part_owned_dofs[1]             = gather_buffer[comm_size - 1];  // max
       part_owned_dofs[2]             = gather_buffer[median_index];   // median
       PetscReal part_owned_dof_ratio = (PetscReal)part_owned_dofs[1] / (PetscReal)part_owned_dofs[2];
-      PetscCall(PetscPrintf(
-          comm, "  Global Vector %" PetscInt_FMT "-DoF nodes          : %" PetscInt_FMT ", %" PetscInt_FMT ", %" PetscInt_FMT ", %f\n", num_comp_q,
-          part_owned_dofs[0] / num_comp_q, part_owned_dofs[1] / num_comp_q, part_owned_dofs[2] / num_comp_q, part_owned_dof_ratio));
+      PetscCall(PetscPrintf(comm,
+                            "  Global Vector %" PetscInt_FMT "-DoF nodes          : %" PetscInt_FMT ", %" PetscInt_FMT ", %" PetscInt_FMT ", %f\n",
+                            num_comp_q, part_owned_dofs[0] / num_comp_q, part_owned_dofs[1] / num_comp_q, part_owned_dofs[2] / num_comp_q,
+                            part_owned_dof_ratio));
     }
 
     PetscCallMPI(MPI_Gather(&local_dofs, 1, MPIU_INT, gather_buffer, 1, MPIU_INT, 0, comm));
@@ -155,9 +155,10 @@ PetscErrorCode DMPrintVecSizes(DM dm, AppCtx *user) {
       part_local_dofs[1]             = gather_buffer[comm_size - 1];  // max
       part_local_dofs[2]             = gather_buffer[median_index];   // median
       PetscReal part_local_dof_ratio = (PetscReal)part_local_dofs[1] / (PetscReal)part_local_dofs[2];
-      PetscCall(PetscPrintf(
-          comm, "  Local Vector %" PetscInt_FMT "-DoF nodes           : %" PetscInt_FMT ", %" PetscInt_FMT ", %" PetscInt_FMT ", %f\n", num_comp_q,
-          part_local_dofs[0] / num_comp_q, part_local_dofs[1] / num_comp_q, part_local_dofs[2] / num_comp_q, part_local_dof_ratio));
+      PetscCall(PetscPrintf(comm,
+                            "  Local Vector %" PetscInt_FMT "-DoF nodes           : %" PetscInt_FMT ", %" PetscInt_FMT ", %" PetscInt_FMT ", %f\n",
+                            num_comp_q, part_local_dofs[0] / num_comp_q, part_local_dofs[1] / num_comp_q, part_local_dofs[2] / num_comp_q,
+                            part_local_dof_ratio));
     }
 
     if (comm_size != 1) {
@@ -189,10 +190,10 @@ PetscErrorCode DMPrintVecSizes(DM dm, AppCtx *user) {
         part_boundary_dofs[1]           = gather_buffer[comm_size - 1];  // max
         part_boundary_dofs[2]           = gather_buffer[median_index];   // median
         PetscReal part_shared_dof_ratio = (PetscReal)part_boundary_dofs[1] / (PetscReal)part_boundary_dofs[2];
-        PetscCall(PetscPrintf(
-            comm, "  Ghost Interface %" PetscInt_FMT "-DoF nodes        : %" PetscInt_FMT ", %" PetscInt_FMT ", %" PetscInt_FMT ", %f\n",
-            num_comp_q, part_boundary_dofs[0] / num_comp_q, part_boundary_dofs[1] / num_comp_q, part_boundary_dofs[2] / num_comp_q,
-            part_shared_dof_ratio));
+        PetscCall(PetscPrintf(comm,
+                              "  Ghost Interface %" PetscInt_FMT "-DoF nodes        : %" PetscInt_FMT ", %" PetscInt_FMT ", %" PetscInt_FMT ", %f\n",
+                              num_comp_q, part_boundary_dofs[0] / num_comp_q, part_boundary_dofs[1] / num_comp_q, part_boundary_dofs[2] / num_comp_q,
+                              part_shared_dof_ratio));
       }
 
       PetscCallMPI(MPI_Gather(&num_ghost_interface_ranks, 1, MPIU_INT, gather_buffer, 1, MPIU_INT, 0, comm));
@@ -213,10 +214,10 @@ PetscErrorCode DMPrintVecSizes(DM dm, AppCtx *user) {
         part_boundary_dofs[1]           = gather_buffer[comm_size - 1];  // max
         part_boundary_dofs[2]           = gather_buffer[median_index];   // median
         PetscReal part_shared_dof_ratio = (PetscReal)part_boundary_dofs[1] / (PetscReal)part_boundary_dofs[2];
-        PetscCall(PetscPrintf(
-            comm, "  Owned Interface %" PetscInt_FMT "-DoF nodes        : %" PetscInt_FMT ", %" PetscInt_FMT ", %" PetscInt_FMT ", %f\n",
-            num_comp_q, part_boundary_dofs[0] / num_comp_q, part_boundary_dofs[1] / num_comp_q, part_boundary_dofs[2] / num_comp_q,
-            part_shared_dof_ratio));
+        PetscCall(PetscPrintf(comm,
+                              "  Owned Interface %" PetscInt_FMT "-DoF nodes        : %" PetscInt_FMT ", %" PetscInt_FMT ", %" PetscInt_FMT ", %f\n",
+                              num_comp_q, part_boundary_dofs[0] / num_comp_q, part_boundary_dofs[1] / num_comp_q, part_boundary_dofs[2] / num_comp_q,
+                              part_shared_dof_ratio));
       }
 
       PetscCallMPI(MPI_Gather(&num_owned_interface_ranks, 1, MPIU_INT, gather_buffer, 1, MPIU_INT, 0, comm));
@@ -236,11 +237,9 @@ PetscErrorCode DMPrintVecSizes(DM dm, AppCtx *user) {
   PetscFunctionReturn(PETSC_SUCCESS);
 }
 
-
-int main(int argc, char **argv)
-{
-  DM       dm;
-  AppCtx   user;
+int main(int argc, char **argv) {
+  DM     dm;
+  AppCtx user;
 
   PetscFunctionBeginUser;
   PetscCall(PetscInitialize(&argc, &argv, NULL, help));
@@ -260,15 +259,14 @@ int main(int argc, char **argv)
 
     PetscCall(DMCreateLocalVector(dm, &vec));
     if (user.project_solution) {
-      PetscErrorCode (*funcs)(PetscInt dim, PetscReal time, const PetscReal x[], PetscInt Nc, PetscScalar *u,
-                              void *ctx) = {project_function};
+      PetscErrorCode (*funcs)(PetscInt dim, PetscReal time, const PetscReal x[], PetscInt Nc, PetscScalar *u, void *ctx) = {project_function};
       PetscCall(DMProjectFunctionLocal(dm, 0, &funcs, NULL, INSERT_VALUES, vec));
     }
     PetscCall(VecViewFromOptions(vec, NULL, "-vec_view"));
     PetscCall(VecDestroy(&vec));
   }
 
-  { // Get time taken to write file
+  {  // Get time taken to write file
     PetscLogEvent      vec_view_log;
     PetscEventPerfInfo perf_info;
 
